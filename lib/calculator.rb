fbbc6f9c4b341fb5d76475cb2ec058fd50b20f44
Sam Morgan <s_morgan@me.com>
2017-05-11 10:33:02 +0100

    Implement #subtract
    - Takes two arguments and subtracts the `number_to_subtract` from the `number`

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,7 @@
 def add(number_1, number_2)
   number_1 + number_2
 end
+
+def subtract(number, number_to_subtract)
+  number - number_to_subtract
+end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'calculator'
 
 RSpec.describe 'a calculator' do
-  it "adds two numbers together" do
+  it 'adds two numbers together' do
     expect(add(2, 3)).to eq 5
     expect(add(1, 2)).to eq 3
     expect(add(0, 1)).to eq 1
@@ -9,6 +9,11 @@ RSpec.describe 'a calculator' do
     expect(add(-1, -1)).to eq -2
     expect(add(0, Float::INFINITY)).to eq Float::INFINITY
     expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+  end
+
+  it 'subtracts one number from another' do
+    expect(subtract(2, 1)).to eq 1
+    expect(subtract(3, 1)).to eq 2
   end
 end
 
