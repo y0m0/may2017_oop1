12d9876988bef7efcedfa4f25b36c3765c1934b7
y0m0 <lixi.daniele@gmail.com>
2017-05-11 19:31:25 +0100

    Adds new class that handle the printing Adds new class which contain the calculator logic

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,18 @@
-def add(number_1, number_2)
-  number_1 + number_2
+# understands how to add and subtract numbers
+class Calculator
+
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+
+  def subtract(number, number_to_subtract)
+    number - number_to_subtract
+  end
 end
 
-def subtract(number, number_to_subtract)
-  number - number_to_subtract
+# understand how to print the output of whatever is passed to it
+class Printer
+  def print(output)
+    "Output: #{output}"
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,24 +1,40 @@
 require 'calculator'
 
-RSpec.describe 'a calculator' do
-  it 'adds two numbers together' do
-    expect(add(2, 3)).to eq 5
-    expect(add(1, 2)).to eq 3
-    expect(add(0, 1)).to eq 1
-    expect(add(-1, 2)).to eq 1
-    expect(add(-1, -1)).to eq -2
-    expect(add(0, Float::INFINITY)).to eq Float::INFINITY
-    expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+RSpec.describe Calculator do
+  # define the subject of the test more explicitly
+  subject(:calculator) { Calculator.new }
+
+  describe "#add" do
+    it 'adds two numbers together' do
+      expect(calculator.add(2, 3)).to eq 5
+      expect(calculator.add(1, 2)).to eq 3
+      expect(calculator.add(0, 1)).to eq 1
+      expect(calculator.add(-1, 2)).to eq 1
+      expect(calculator.add(-1, -1)).to eq -2
+      expect(calculator.add(0, Float::INFINITY)).to eq Float::INFINITY
+      expect(calculator.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+    end
   end
 
-  it 'subtracts one number from another' do
-    expect(subtract(2, 1)).to eq 1
-    expect(subtract(3, 1)).to eq 2
-    expect(subtract(0, 0)).to eq 0
-    expect(subtract(0, 1)).to eq -1
-    expect(subtract(-1, -1)).to eq 0
-    expect(subtract(0, Float::INFINITY)).to eq -Float::INFINITY
+  describe "#subtract" do
+    it 'subtracts one number from another' do
+      expect(calculator.subtract(2, 1)).to eq 1
+      expect(calculator.subtract(3, 1)).to eq 2
+      expect(calculator.subtract(0, 0)).to eq 0
+      expect(calculator.subtract(0, 1)).to eq -1
+      expect(calculator.subtract(-1, -1)).to eq 0
+      expect(calculator.subtract(0, Float::INFINITY)).to eq -Float::INFINITY
+    end
   end
 end
 
+describe Printer do
+  subject(:printer) { Printer.new }
 
+  describe "#print" do
+    it 'it prints the ouput' do
+      string = "this is a test"
+      expect(printer.print("this is a test")).to eq "Output: " + string
+    end
+  end
+end
