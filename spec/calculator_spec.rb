88056b9a7b4ff394ab8fdc748ddccea84ad522bd
Sam Morgan <s_morgan@me.com>
2017-05-11 10:28:31 +0100

    More tests for #add

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe 'a calculator' do
   it "adds two numbers together" do
     expect(add(2, 3)).to eq 5
     expect(add(1, 2)).to eq 3
+    expect(add(0, 1)).to eq 1
+    expect(add(-1, 2)).to eq 1
+    expect(add(-1, -1)).to eq -2
+    expect(add(0, Float::INFINITY)).to eq Float::INFINITY
+    expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
   end
 end
 
